35e9cd38a6c91e76e9b46ab523092f6ee1d7d5a3
zeynepaltay <altay-zeynep@hotmail.com>
2025-10-06 00:23:00 +0300

    findByEmail added

--- src/main/java/Case/Study/DIGITOPIA/repositories/UserRepository.java
+++ src/main/java/Case/Study/DIGITOPIA/repositories/UserRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Page<User> findByNormalizedNameContainingIgnoreCase(String normalizedName, Pageable pageable);
+    Optional<User> findByEmail(String email);
 }
