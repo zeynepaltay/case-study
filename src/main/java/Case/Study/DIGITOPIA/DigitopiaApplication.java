f7ddc62a9d7ea1eba128a8fbc66a602289e614c7
zeynepaltay <altay-zeynep@hotmail.com>
2025-10-06 23:00:37 +0300

    @EnableScheduling annotation added

--- src/main/java/Case/Study/DIGITOPIA/DigitopiaApplication.java
+++ src/main/java/Case/Study/DIGITOPIA/DigitopiaApplication.java
@@ -2,8 +2,10 @@ package Case.Study.DIGITOPIA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DigitopiaApplication {
 
 	public static void main(String[] args) {
