e4e6508a445f483c5a284a963de836ea48ef86e7
zeynepaltay <altay-zeynep@hotmail.com>
2025-10-06 23:06:07 +0300

    createdAt and createdBy deleted

--- src/main/java/Case/Study/DIGITOPIA/dtos/responses/InvitationResponse.java
+++ src/main/java/Case/Study/DIGITOPIA/dtos/responses/InvitationResponse.java
@@ -5,20 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
-
-import java.util.Date;
 import java.util.UUID;
 
 @Getter
 @Builder
 @AllArgsConstructor
 public class InvitationResponse {
-    private Long userId;
-    private Long organizationId;
+    private UUID userId;
+    private UUID organizationId;
     private String invitationMessage;
-    private Date createdAt;//??
-    private Date updatedAt;
-    private UUID createdBy;
-    private UUID updatedBy;
     private InvitationStatus invitationStatus;
 }
--- src/main/java/Case/Study/DIGITOPIA/dtos/responses/OrganizationResponse.java
+++ src/main/java/Case/Study/DIGITOPIA/dtos/responses/OrganizationResponse.java
@@ -5,25 +5,16 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
-
-import java.util.Date;
-import java.util.HashSet;
 import java.util.Set;
-import java.util.UUID;
 
 @Getter
 @Builder
 @AllArgsConstructor
 public class OrganizationResponse {
     private String name;
-    private String normalizedName;//???
     private String registryNumber;
     private String email;
     private Integer companySize;
     private Integer foundationYear;
-    private Date createdAt;// kalmalı mı?
-    private Date updatedAt;
-    private UUID createdBy;
-    private UUID updatedBy;
-    private Set<User> user = new HashSet<>();
+    private Set<User> user;
 }
