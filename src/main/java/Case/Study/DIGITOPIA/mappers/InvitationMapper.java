5da2cc91e8b5ed98722ce5c1041db64363e85a8c
zeynepaltay <altay-zeynep@hotmail.com>
2025-10-06 22:57:32 +0300

    toEntity and toResponse methods added

--- src/main/java/Case/Study/DIGITOPIA/mappers/InvitationMapper.java
+++ src/main/java/Case/Study/DIGITOPIA/mappers/InvitationMapper.java
@@ -1,7 +1,12 @@
 package Case.Study.DIGITOPIA.mappers;
 
+import Case.Study.DIGITOPIA.dtos.requests.InvitationRequest;
+import Case.Study.DIGITOPIA.dtos.responses.InvitationResponse;
+import Case.Study.DIGITOPIA.models.Invitation;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface InvitationMapper {
+    Invitation toEntity(InvitationRequest request);
+    InvitationResponse toResponse(Invitation invitation);
 }
