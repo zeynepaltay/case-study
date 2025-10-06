7cadb28127781c3026546f5d28f71942b14596b7
zeynepaltay <altay-zeynep@hotmail.com>
2025-10-06 23:08:05 +0300

    job created for 7 days count

---
+++ src/main/java/Case/Study/DIGITOPIA/schedulers/InvitationScheduler.java
@@ -0,0 +1,28 @@
+package Case.Study.DIGITOPIA.schedulers;
+
+import Case.Study.DIGITOPIA.models.Invitation;
+import Case.Study.DIGITOPIA.models.enums.InvitationStatus;
+import Case.Study.DIGITOPIA.repositories.InvitationRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+@AllArgsConstructor
+public class InvitationScheduler {
+    private final InvitationRepository invitationRepository;
+
+    @Scheduled(cron = "0 0 0 * * *")
+    @Transactional
+    public void scheduleInvitation() {
+    LocalDateTime sevenDaysAgo = LocalDateTime.now().minusDays(7);
+    List<Invitation> oldInvitations = invitationRepository.findByCreatedAtBeforeAndStatusNot(sevenDaysAgo, InvitationStatus.EXPIRED);
+    oldInvitations.forEach(invitation -> invitation.setInvitationStatus(InvitationStatus.EXPIRED));
+    invitationRepository.saveAll(oldInvitations);
+    System.out.println(oldInvitations.size() + " invitation(s) updated to EXPIRED at " + LocalDateTime.now());
+    }
+}
