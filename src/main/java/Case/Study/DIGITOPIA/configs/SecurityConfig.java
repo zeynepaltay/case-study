ffb5dad3c23cea0a3da2f0c22de59e44e2292d5a
zeynepaltay <altay-zeynep@hotmail.com>
2025-10-08 23:22:15 +0300

    security config added

---
+++ src/main/java/Case/Study/DIGITOPIA/configs/SecurityConfig.java
@@ -0,0 +1,28 @@
+package Case.Study.DIGITOPIA.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/public/**").permitAll()
+                        .requestMatchers("/api/admin/**").hasRole("MANAGER")
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(jwt -> {})
+                );
+
+        return http.build();
+    }
+}
