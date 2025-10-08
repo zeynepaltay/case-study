83113332f88832a9c074e8e89acf8ab93f8992b8
zeynepaltay <altay-zeynep@hotmail.com>
2025-10-08 23:25:14 +0300

    toEnitty added

--- src/main/java/Case/Study/DIGITOPIA/mappers/OrganizationMapper.java
+++ src/main/java/Case/Study/DIGITOPIA/mappers/OrganizationMapper.java
@@ -5,11 +5,14 @@ import Case.Study.DIGITOPIA.dtos.responses.OrganizationResponse;
 import Case.Study.DIGITOPIA.models.Organization;
 import org.mapstruct.Mapper;
 
+import java.util.List;
 import java.util.Set;
 
 
 @Mapper(componentModel = "spring")
 public interface OrganizationMapper {
     Organization toEntity(OrganizationRequest request);
+    OrganizationResponse toResponse(Organization organizations);
     Set<OrganizationResponse> toResponseSet(Set<Organization> organizations);
+    List<OrganizationResponse> toResponseList(List<Organization> organizations);
 }
--- src/main/java/Case/Study/DIGITOPIA/mappers/UserMapper.java
+++ src/main/java/Case/Study/DIGITOPIA/mappers/UserMapper.java
@@ -6,6 +6,7 @@ import Case.Study.DIGITOPIA.models.User;
 import org.mapstruct.Mapper;
 
 import java.util.List;
+import java.util.Set;
 
 
 @Mapper(componentModel = "spring", uses = {OrganizationMapper.class})
@@ -13,4 +14,5 @@ public interface UserMapper {
     User toEntity(UserRequest request);
     UserResponse toResponse(User users);
     List<UserResponse> toResponseList(List<User> users);
+    Set<UserResponse> toResponseSet(Set<User> users);//TODO bu ve bi üstteki aynı şey???
 }
