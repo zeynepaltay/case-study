ff453014469ee02113241af2c8a8b0d53fed40a1
zeynepaltay <altay-zeynep@hotmail.com>
2025-10-10 19:46:33 +0300

    toEntity method updated

--- src/main/java/Case/Study/DIGITOPIA/mappers/OrganizationMapper.java
+++ src/main/java/Case/Study/DIGITOPIA/mappers/OrganizationMapper.java
@@ -3,14 +3,42 @@ package Case.Study.DIGITOPIA.mappers;
 import Case.Study.DIGITOPIA.dtos.requests.OrganizationRequest;
 import Case.Study.DIGITOPIA.dtos.responses.OrganizationResponse;
 import Case.Study.DIGITOPIA.models.Organization;
+import Case.Study.DIGITOPIA.models.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 
 @Mapper(componentModel = "spring")
 public interface OrganizationMapper {
     Organization toEntity(OrganizationRequest request);
-    OrganizationResponse toResponse(Organization organizations);
+
+    @Mapping(source = "users", target = "userId")
+    OrganizationResponse toResponse(Organization organization);
+
     List<OrganizationResponse> toResponseList(List<Organization> organizations);
+
+    default List<UUID> mapUsersToUserIds(List<User> users) {
+        if (users == null || users.isEmpty()) {
+            return Collections.emptyList();
+        }
+        return users.stream()
+                .map(User::getID)
+                .collect(Collectors.toList());
+    }
+
+    default Organization toEntity(UUID id) {
+        if (id == null) return null;
+        Organization organization = new Organization();
+        organization.setID(id);
+        return organization;
+    }
+
+    default UUID toId(Organization organization) {
+        return organization != null ? organization.getID() : null;
+    }
 }
