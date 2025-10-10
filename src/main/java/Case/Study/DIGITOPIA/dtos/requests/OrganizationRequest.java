75f3ce67b37e16a6e29a1753fea81a1d7fbb97ae
zeynepaltay <altay-zeynep@hotmail.com>
2025-10-10 19:47:00 +0300

    column name added

--- src/main/java/Case/Study/DIGITOPIA/dtos/requests/OrganizationRequest.java
+++ src/main/java/Case/Study/DIGITOPIA/dtos/requests/OrganizationRequest.java
@@ -1,8 +1,7 @@
 package Case.Study.DIGITOPIA.dtos.requests;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 import java.util.List;
@@ -17,14 +16,19 @@ public class OrganizationRequest {
     @NotBlank(message = "Organization name can not be left blank")
     @Pattern(regexp = "[A-Za-z0-9]+$", message = "Must contain only letters and numbers")
     private String name;
-    // TODO bunu silcem full nameden kendim üretip işliycem sonra
     @NotBlank(message = "Registry number can not be left blank")
     @Pattern(regexp = "[A-Za-z0-9]+$", message = "Must contain only letters and numbers")
+    @Column(name = "registry_number", unique = true)
     private String registryNumber;
     @NotBlank(message = "Email must not be blank")
     @Email(message = "Email should be valid")
     private String email;
+    @NotNull(message = "Company size cannot be null")
+    @Positive(message = "Company size must be a positive number")
     private Integer companySize;
+    @NotNull(message = "Foundation year cannot be null")
+    @Min(value = 1900, message = "Foundation year must be after 1900")
+    @Max(value = 2025, message = "Foundation year cannot be in the future")
     private Integer foundationYear;
     private List<UUID> userId;
 }
