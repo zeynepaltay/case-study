0e5781487e04645dcb35308ef396e507a773e3df
zeynepaltay <altay-zeynep@hotmail.com>
2025-10-10 19:47:37 +0300

    mapping annotation added to toResponse method

--- src/main/java/Case/Study/DIGITOPIA/mappers/UserMapper.java
+++ src/main/java/Case/Study/DIGITOPIA/mappers/UserMapper.java
@@ -2,15 +2,32 @@ package Case.Study.DIGITOPIA.mappers;
 
 import Case.Study.DIGITOPIA.dtos.requests.UserRequest;
 import Case.Study.DIGITOPIA.dtos.responses.UserResponse;
+import Case.Study.DIGITOPIA.models.Organization;
 import Case.Study.DIGITOPIA.models.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+import java.util.Collections;
 import java.util.List;
-
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring", uses = {OrganizationMapper.class})
 public interface UserMapper {
+    @Mapping(target = "organization", source = "organizationId")
     User toEntity(UserRequest request);
-    UserResponse toResponse(User users);
+
+    @Mapping(source = "organization", target = "organizationId")
+    UserResponse toResponse(User user);
+
     List<UserResponse> toResponseList(List<User> users);
+
+    default List<UUID> mapOrganizationsToOrganizationIds(List<Organization> organizations) {
+        if (organizations == null || organizations.isEmpty()) {
+            return Collections.emptyList();
+        }
+        return organizations.stream()
+                .map(Organization::getID)
+                .collect(Collectors.toList());
+    }
 }
